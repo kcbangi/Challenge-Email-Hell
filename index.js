c0325db7834aec2977fa4aa812719bff6361e567
Keith <kayzee@live.com>
2019-07-20 10:26:10 -1000

    getting first line error

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const emailLog = require('./email_log.json');
+const emailLog = require("./email_log.json");
 
 function countUniqueEmails(emailFile) {
-  // do work here
+  let list = emailFile.emails;
+
+  let emails = list.reduce(function(previous, current) {
+    let email = current.email;
+    if (!previous[email]) {
+      previous[email] = 1;
+    } else {
+      previous[email] += 1;
+    }
+    return previous;
+  });
+
+  return emails;
 }
 
 console.log(countUniqueEmails(emailLog));
